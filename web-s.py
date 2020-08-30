6513ae387cd2b78d05c1814fe3753d02836a1248
Sharna Hossain <sharnajh@gmail.com>
2020-08-30 14:39:53 -0400

    fixed year

--- web-s.py
+++ web-s.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+year = 2021
+
 driver = webdriver.Chrome("./chromedriver")
-driver.get("https://www.bmcc.cuny.edu/academics/academic-calendar/spring-2021/")
+driver.get("https://www.bmcc.cuny.edu/academics/academic-calendar/winter-2021/")
 content = driver.page_source
 soup = BeautifulSoup(content)
 start_dates = []
@@ -17,7 +19,7 @@ for date in soup.find_all("td", attrs={"class": "column-1"}):
         start = b[0].strip().split(" ")
         start_month = time.strptime(start[0], "%B").tm_mon
         start_day = start[1]
-        start_date = f'{start_month}/{start_day}/2020'
+        start_date = f'{start_month}/{start_day}/{year}'
 
         end = b[1].strip().split(" ")
         if not end[0].isnumeric():
@@ -26,12 +28,12 @@ for date in soup.find_all("td", attrs={"class": "column-1"}):
         else:
             end_month = start_month
             end_day = end[0]
-        end_date = f'{end_month}/{end_day}/2020'
+        end_date = f'{end_month}/{end_day}/{year}'
     else:
         m = data.split(" ")
         month = time.strptime(m[0], "%B").tm_mon
         day = m[1]
-        start_date = end_date = f'{month}/{day}/2020'
+        start_date = end_date = f'{month}/{day}/{year}'
     start_dates.append(start_date)
     end_dates.append(end_date)
 
@@ -40,4 +42,4 @@ for event in soup.find_all("td", attrs={"class": "column-3"}):
 
 df = pd.DataFrame({'Subject': subjects, 'Start Date': start_dates,
                    'Start Time': "12:00 AM", 'End Date': end_dates, 'End Time': "11:59 PM", "All day event": "TRUE", "Description": "", "Location": ""})
-df.to_csv('spring2021_calendar.csv', index=False, encoding='utf-8')
+df.to_csv('winter2021_calendar.csv', index=False, encoding='utf-8')
